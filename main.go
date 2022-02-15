c1ca0da58a4e2fb70038abc991cf1c82ef0bfc2a
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-02-15 17:24:35 +0100

    Node checker

--- main.go
+++ main.go
@@ -1,55 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInvalidNode = errors.New("Node is not valid")
+
+type Node interface {
+	SetValue(v int) error
+	GetValue() int
+}
 
 //type SLLNode
 type SLLNode struct {
-	next  *SLLNode
-	value int
+	next         *SLLNode
+	value        int
+	SNodeMessage string
 }
 
-func (sNode *SLLNode) SetValue(v int) {
+func (sNode *SLLNode) SetValue(v int) error {
+	if sNode == nil {
+		return ErrInvalidNode
+	}
 	sNode.value = v
+	return nil
 }
 
 func (sNode *SLLNode) GetValue() int {
 	return sNode.value
 }
 
-type SingleLinkedList struct {
-	head *SLLNode
-	tail *SLLNode
+func NewSLLNode() *SLLNode {
+	return &SLLNode{SNodeMessage: "This is a message from the normal Node"}
 }
 
-func newSingleLinkedList() *SingleLinkedList {
-	return new(SingleLinkedList)
+//type PowerNode
+type PowerNode struct {
+	next         *PowerNode
+	value        int
+	PNodeMessage string
 }
 
-func (list *SingleLinkedList) Add(v int) {
-	newNode := &SLLNode{value: v}
-	if list.head == nil {
-		list.head = newNode
-	} else if list.tail == list.head {
-		list.head.next = newNode
-	} else if list.tail != nil {
-		list.tail.next = newNode
+func (sNode *PowerNode) SetValue(v int) error {
+	if sNode == nil {
+		return ErrInvalidNode
 	}
-	list.tail = newNode
+	sNode.value = v * 10
+	return nil
 }
 
-func (list *SingleLinkedList) String() string {
-	s := ""
-	for n := list.head; n != nil; n = n.next {
-		s += fmt.Sprintf("{%d} ", n.GetValue())
-	}
-	return s
+func (sNode *PowerNode) GetValue() int {
+	return sNode.value
+}
+
+func NewPowerNode() *PowerNode {
+	return &PowerNode{PNodeMessage: "This is a message from the normal Node"}
 }
 
 func main() {
-	list := newSingleLinkedList()
-	list.Add(3)
-	list.Add(4)
-	list.Add(5)
-	list.Add(6)
-	fmt.Println("Hello, playground", list)
+
+	n := createNode(5)
+
+	switch concreten := n.(type) {
+	case *SLLNode:
+		fmt.Println("Type is SLLNode, message:", concreten.SNodeMessage)
+	case *PowerNode:
+		fmt.Println("Type is PowerNode, message:", concreten.PNodeMessage)
+	}
+
 }
+
+func createNode(v int) Node {
+	sn := NewPowerNode()
+	sn.SetValue(v)
+	return sn
+}
+
+// type SingleLinkedList struct {
+// 	head *SLLNode
+// 	tail *SLLNode
+// }
+
+// func newSingleLinkedList() *SingleLinkedList {
+// 	return new(SingleLinkedList)
+// }
+
+// func (list *SingleLinkedList) Add(v int) {
+// 	newNode := &SLLNode{value: v}
+// 	if list.head == nil {
+// 		list.head = newNode
+// 	} else if list.tail == list.head {
+// 		list.head.next = newNode
+// 	} else if list.tail != nil {
+// 		list.tail.next = newNode
+// 	}
+// 	list.tail = newNode
+// }
+
+// func (list *SingleLinkedList) String() string {
+// 	s := ""
+// 	for n := list.head; n != nil; n = n.next {
+// 		s += fmt.Sprintf("{%d} ", n.GetValue())
+// 	}
+// 	return s
+// }
