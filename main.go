cf4d175d2de8d9b6579d8d6c801fa5355c8ef3e7
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-02-17 17:31:07 +0100

    SLLLink update 1.2

--- main.go
+++ main.go
@@ -55,11 +55,32 @@ func (sNode *PowerNode) GetValue() int {
 }
 
 func NewPowerNode() *PowerNode {
-	return &PowerNode{PNodeMessage: "This is a message from the normal Node"}
+	return &PowerNode{PNodeMessage: "This is a message from the power Node"}
 }
 
-func main() {
+// func main() {
+// 	var n Node
+// 	n = &SLLNode{}
+// 	fmt.Println(n.SetValue(4))
+// }
 
+// func main() {
+// 	printType("text")
+// 	printType(3)
+// 	printType(4.0)
+// }
+
+// func printType(i interface{}) {
+// 	switch i := i.(type) {
+// 	case string:
+// 		fmt.Println("This is a string type", i)
+// 	case int:
+// 		fmt.Println("This is an int type", i)
+// 	case float32:
+// 		fmt.Println("This is a float type", i)
+// 	}
+// }
+func main() {
 	n := createNode(5)
 
 	switch concreten := n.(type) {
@@ -68,11 +89,11 @@ func main() {
 	case *PowerNode:
 		fmt.Println("Type is PowerNode, message:", concreten.PNodeMessage)
 	}
-
+	sNode := &SLLNode{}
+	n = sNode
 }
-
 func createNode(v int) Node {
-	sn := NewPowerNode()
+	sn := NewSLLNode()
 	sn.SetValue(v)
 	return sn
 }
@@ -104,4 +125,38 @@ func createNode(v int) Node {
 // 		s += fmt.Sprintf("{%d} ", n.GetValue())
 // 	}
 // 	return s
+// }
+
+// type magicStore struct {
+// 	value interface{}
+// 	name  string
+// }
+
+// func (ms *magicStore) SetValue(v interface{}) {
+// 	ms.value = v
+// }
+
+// func (ms *magicStore) GetValue() interface{} {
+// 	return ms.value
+// }
+
+// func NewMagicStore(nm string) *magicStore {
+// 	return &magicStore{name: nm}
+// }
+
+// func main() {
+// 	istore := NewMagicStore("Integer Store")
+// 	istore.SetValue(4)
+// 	if v, ok := istore.GetValue().(int); ok {
+// 		v *= 4
+// 		fmt.Println(v)
+// 	}
+
+// 	sstore := NewMagicStore("String store")
+// 	sstore.SetValue("Hello")
+// 	if v, ok := sstore.GetValue().(string); ok {
+// 		v += " World"
+// 		fmt.Println(v)
+// 	}
+
 // }
